1a823216c2a3be858d38420d788ec27ea29ef7a8
ArevaloDev <arevalodaniel999@gmail.com>
2025-03-26 13:25:42 -0600

    creando cards de drivers

--- f1_web/src/app/components/drivers/drivers.component.ts
+++ f1_web/src/app/components/drivers/drivers.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class DriversComponent {
 
+  public datatest = ['nombre1', 'nombre2', 'nombre3', 'nombre4', 'nombre5', 'nombre7', 'nombre8']
 }
